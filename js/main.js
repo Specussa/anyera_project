a4cf7e3306725bf1db60bd115c90bcdf637a4eef
Spec Ussa <serega.ussa@gmail.com>
2023-05-18 15:37:06 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -7,9 +7,8 @@ const burgernav_c = document.querySelector('.header__consultation_burger');
 const overlay = document.querySelector('.overlay');
 const form = document.getElementById('form');
 const username = document.getElementById('username');
-const control = document.getElementById('control');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+const phone = document.getElementById('control');
+const text = document.getElementById('text');
 
 // start navbar
 document.querySelector('.header__burger').addEventListener('click', function() {
@@ -122,35 +121,26 @@ checkInputs();
 function checkInputs() {
 // trim to remove the whitespaces
 const usernameValue = username.value.trim();
-const controlValue = control.value.trim();
-const passwordValue = password.value.trim();
-const password2Value = password2.value.trim();
+const phoneValue = phone.value.trim();
+const textValue = text.value.trim();
 
 if(usernameValue === '') {
-  setErrorFor(username, 'Неверный Логин или Email');
+  setErrorFor(username, 'Обязательное поле');
 } else {
   setSuccessFor(username);
 }
 
-if(controlValue === '') {
-  setErrorFor(control, 'Неверное контрольное слово');
+if(phoneValue === '') {
+  setErrorFor(phone, 'Обязательное поле');
 } else {
-  setSuccessFor(control);
+  setSuccessFor(phone);
 }
 
-if(passwordValue === '') {
-  setErrorFor(password, 'Пароль должен быть не менее 6 символов');
+if(textValue === '') {
+  setErrorFor(text, 'Обязательное поле');
 } else {
-  setSuccessFor(password);
+  setSuccessFor(text);
 }
-
-if(password2Value === '') {
-    setErrorFor(password2, 'Пароли не совпадают');
-  } else if(passwordValue !== password2Value) {
-    setErrorFor(password2, 'Пароли не совпадают');
-  } else{
-    setSuccessFor(password2);
-  }
 }
 
 function setErrorFor(input, message) {
@@ -163,6 +153,10 @@ function setErrorFor(input, message) {
 function setSuccessFor(input) {
   const formControl = input.parentElement;
   formControl.className = 'forms__form_control success';
+}
+
+function iscontrol(phone) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(phone);
 }
 
 // function iscontrol(control) {
